e5abb4b1745d35b1f1a10f5306aafa0774a9ba73
cmperezg <cmperezg@scspc612.cs.uwaterloo.ca>
2016-03-14 13:14:01 -0400

    cuckoo tsp

---
+++ AlgCompCSBio/src/algcomp/alg/CuckooSearchTSP.java
@@ -0,0 +1,150 @@
+package algcomp.alg;
+
+import java.util.Random;
+
+import org.apache.commons.math3.distribution.WeibullDistribution;
+
+
+import algcomp.util.Graph;
+
+public class CuckooSearchTSP {
+	
+
+	
+	Graph g;
+	static Random r;
+	int numpoints;
+	//will use weibull distribution for step length
+	static WeibullDistribution weibull;
+	int numnests; //number of nests
+	double abrate; //abandonment rate
+	int timer; //timer
+	EggTSP[] nests;
+	EggTSP bestegg;
+	
+	public CuckooSearchTSP(Graph _g, int _numnests, double _abrate, int _timer){
+		
+		g = _g;
+		numnests = _numnests;
+		abrate = _abrate;
+		timer = _timer;
+		nests = new EggTSP[numnests];
+		numpoints = g.size();
+		
+		r = new Random();
+		weibull = new WeibullDistribution(1,0.1);
+		
+		bestegg = new EggTSP(numpoints);
+		evaluate(bestegg);
+		
+		for(int i = 0;i<numnests;i++){
+			nests[i] = new EggTSP(numpoints);
+			evaluate(nests[i]);
+			if(nests[i].getEval()<bestegg.getEval()){
+				bestegg = nests[i];
+			}
+			
+		}
+
+	}
+	
+	public EggTSP step(){
+		
+		//generate cuckoos
+		for(int i = 0; i<nests.length; i++){
+			EggTSP cuckoo = new EggTSP(nests[i]);
+			//System.out.println("1"+cuckoo.toString());
+			
+			eggLevyStep(cuckoo);
+			evaluate(cuckoo);
+			//System.out.println("2"+cuckoo.toString());
+			//pick a random nest to dump it and carry out comparisons
+			int ind = r.nextInt(nests.length);
+			evaluate(nests[ind]);
+			//System.out.println("3"+nests[ind].toString());
+			if(cuckoo.getEval()<nests[ind].getEval()){
+				//System.out.println("yay");
+				nests[ind]=new EggTSP(cuckoo);
+				evaluate(nests[ind]);
+				if(cuckoo.getEval()<bestegg.getEval()){
+					bestegg = new EggTSP(cuckoo);
+					evaluate(bestegg);
+				}
+			}else{
+				double rn = r.nextDouble();
+				//abandon it?
+				if(rn < abrate){
+					nests[ind] = new EggTSP(numpoints);
+				}
+			}
+		}
+		//System.out.println(bestegg.toString());
+		return bestegg;
+	}
+	
+	public EggTSP fullrun(){
+		System.out.println("TSP Cuckoo run");
+		
+		
+		final Thread thisThread = Thread.currentThread();
+		final int timeToRun = timer; // 1200 = 2 minutes;
+
+		new Thread(new Runnable() {
+		    public void run() {
+		        try {
+					thisThread.sleep(timeToRun);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					System.out.println("Problem with putting thread to sleep");
+				}
+		        thisThread.interrupt();
+		    }
+		}).start();
+
+		while (!Thread.interrupted()) {
+		    step();
+		}
+		
+		return bestegg;
+	}
+	
+	public double evaluate(EggTSP pc) {
+		double ev = 0.0;
+		int[] path = pc.getPath();
+		//printArray(path);
+		for(int i=0; i<path.length-1;i++){
+			ev = ev + g.getPoint(path[i]).distanceTo(g.getPoint(path[i+1]));
+		}
+		
+		pc.setEval(ev);
+		return ev;
+	}
+	
+	public void eggLevyStep(EggTSP e){
+		double rand = r.nextDouble();
+		double weib = weibull.density(rand);
+		int nummuts = (int)Math.ceil(weib);
+
+		
+		for(int i = 0; i<nummuts; i++){
+			e.mutate();
+		}
+	}
+	
+
+	
+	
+//	public static void main(String[] args){
+//		
+//		for(int i =0; i<100; i++){
+//			r = new Random();
+//			weibull = new WeibullDistribution(1,0.1);
+//			
+//			double d = r.nextInt(2);
+//			System.out.println(d + "," + weibull.density(d));
+//		}
+//		
+//	}
+
+
+}
---
+++ AlgCompCSBio/src/algcomp/alg/EggTSP.java
@@ -0,0 +1,90 @@
+package algcomp.alg;
+
+import java.util.Random;
+
+public class EggTSP {
+	int[] path;
+	double eval;
+	int numpoints;
+	
+	public EggTSP(int _num){
+		numpoints = _num;
+		eval = 0.0;
+		path = new int[numpoints];
+		for(int i = 0; i<numpoints;i++){
+			path[i] = i;
+		}
+		shuffleArray(path);
+	}
+	
+	public EggTSP(EggTSP egg2) {
+		numpoints = egg2.numpoints;
+		eval = 0.0;
+		path = egg2.getPath();
+	}
+
+	public String toString(){
+		return "path: " + printArray(path) + " ev: "+ eval;
+	}
+	
+	private String printArray(int[] arr){
+		String s = "";
+		for(int i = 0;i<arr.length;i++){
+			s = s+(arr[i]+" ");
+		}
+		
+		return s;
+	}
+
+	public double getEval() {
+		return eval;
+	}
+
+	public void setEval(double eval) {
+		this.eval = eval;
+	}
+
+	public int[] getPath() {
+		return path;
+	}
+	public void mutate() {
+		Random random =  new Random();
+		int mutpoint = random.nextInt(numpoints-1);
+		int mutpoint2 = random.nextInt(numpoints-1);
+		int temp = 0;
+		
+//		temp = path[mutpoint];
+//		path[mutpoint] = path[mutpoint2];
+//		path[0] = temp;
+		
+		
+		if(mutpoint == numpoints-1){
+			temp = path[numpoints-1];
+			path[numpoints-1] = path[0];
+			path[0] = temp;
+		}else{
+			temp = path[mutpoint];
+			path[mutpoint] = path[mutpoint+1];
+			path[mutpoint+1] = temp;
+		}
+		
+	}
+	
+	// Fisher–Yates shuffle array function
+			private void shuffleArray(int[] array)
+			{
+			    int index;
+			    Random random = new Random();
+			    for (int i = array.length - 1; i > 0; i--)
+			    {
+			        index = random.nextInt(i + 1);
+			        if (index != i)
+			        {
+			            array[index] ^= array[i];
+			            array[i] ^= array[index];
+			            array[index] ^= array[i];
+			        }
+			    }
+			}
+	
+}
